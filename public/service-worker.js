3626deaf95deb8b8c22edf7f13de2f9f8b125e6d
Norman Cerros <n_cerros@hotmail.com>
2021-06-10 19:47:57 -0600

    modified service-worker.js and created favicom file

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,3 +41,39 @@ self.addEventListener("activate", function (evt) {
   
     self.clients.claim();
   });
+  // when the fetch handler get a response, it will create a new one  and 
+ //   store it in the cache
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+            // From the cache can be requested in case the requesr fails. 
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+    evt.respondWith(
+        fetch(evt.request).catch(function () {
+          return caches.match(evt.request).then(function (response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get("accept").includes("text/html")) {
+              return caches.match("/");
+            }
+          });
+        })
+      );
+    });
+    
